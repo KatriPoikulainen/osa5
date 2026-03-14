34584ab6e0243b5ef69fab694cb9cd40aebded59
Katri Poikulainen <katri.poikulainen@student.hamk.fi>
2026-03-14 19:44:28 +0200

    blogilistan testit step2

--- bloglist-frontend/src/components/Blog.test.jsx
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react'
 import { test, expect } from 'vitest'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders title and author', () => {
+test('url, likes and user are shown when view button is clicked', async () => {
   const blog = {
     title: 'Testing React',
     author: 'Katri Virtanen',
@@ -16,6 +17,11 @@ test('renders title and author', () => {
 
   render(<Blog blog={blog} likeBlog={() => {}} removeBlog={() => {}} user={{ username: 'katri' }} />)
 
-  const element = screen.getByText('Testing React Katri Virtanen')
-  expect(element).toBeDefined()
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  screen.getByText('http://example.com')
+  screen.getByText('likes 5')
+  screen.getByText('Katri Virtanen')
 })
